70f75ee8481ad43fd857caa5c18ec53e7f2b77fb
Abhishek Kumar Pandey <abhishek.kumar.pandey@ericsson.com>
2020-07-10 22:01:44 +0530

    allfiles

--- src/main/java/com/sample/spring/Resource.java
+++ src/main/java/com/sample/spring/Resource.java
@@ -1,4 +1,15 @@
 package com.sample.spring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class Resource {
+
+    @GetMapping
+    public String test() {
+        return "Hello World";
+    }
 }
